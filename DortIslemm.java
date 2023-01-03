1a1022a7611d45e9aa36ca93c04087d8181a9df3
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-03 18:39:13 +0300

    Add files via upload

---
+++ DortIslemm.java
@@ -0,0 +1,9 @@
+package kodlamahmwrk;
+
+public class DortIslemm {
+    public static void main(String[] args) {
+       DortIslem dortIslem=new DortIslem();
+       int sonuc=dortIslem.Topla(3,4);
+        System.out.println(sonuc);
+    }
+}
