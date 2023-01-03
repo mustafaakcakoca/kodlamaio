34c5486d0dbc7ef05c9e0ddb2c154c1d0e601f2f
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-03 18:09:19 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package kodlamahmwrk;
+
+import java.util.Objects;
+
+public class main {
+    public static void main(String[] args) {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+    }
+}
