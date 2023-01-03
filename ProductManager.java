24cf231440f1559608e66b4fa7f237cdef805b27
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-03 20:06:50 +0300

    Add files via upload

---
+++ ProductManager.java
@@ -0,0 +1,10 @@
+package kodlamahmwrk;
+
+public class ProductManager {
+    public void Add(Product product){
+        //JDBC
+        System.out.println("Ürün Eklendi : "+product.get_name(""));
+
+    }
+
+}
