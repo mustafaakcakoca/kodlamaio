2171792f431398e21d68afae9a9f66f63fd214a3
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-03 20:17:22 +0300

    Add files via upload

---
+++ methodOverloading.java
@@ -0,0 +1,10 @@
+package kodlamahmwrk;
+
+public class methodOverloading {
+    public static void main(String[] args) {
+    DortIslem dortIslem=new DortIslem();
+        System.out.println(dortIslem.topla(2,3));
+
+    }
+
+}
