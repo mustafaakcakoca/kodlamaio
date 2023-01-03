9f37a77278dad93bf9c988273b26f64de74a00e4
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-03 20:06:29 +0300

    Update Product1.java

--- Product1.java
+++ Product1.java
@@ -3,12 +3,13 @@ package kodlamahmwrk;
 public class Product1 {
     public static void main(String[] args) {
         Product product=new Product();
-        product.id=1;
-        product.name="Laptop";
-        product.description="Asus Laptop";
-        product.price=5000;
-        product.stockAmount=3;
-        System.out.println(product.name);
+        product.setId(1);
+        product.set_name("Laptop");
+        product.set_description("Asus Laptop");
+        product.set_price(5000);
+        product.set_stockAmount(3);
+        System.out.println(product.get_name("Laptop"));
+        System.out.println(product.get_kod());
 
         ProductManager productManager=new ProductManager();
         productManager.Add(product);
